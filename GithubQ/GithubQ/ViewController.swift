6b8f717d64a81d764a91e019ec16526776f5927f
Tony Hadisiswanto <tony.hadisiswanto@gmail.com>
2020-11-15 12:05:05 +0700

    First init

---
+++ GithubQ/GithubQ/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GithubQ
+//
+//  Created by Tony Hadisiswanto on 15/11/20.
+//  Copyright © 2020 inotnesia. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
